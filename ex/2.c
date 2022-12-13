caf1eef9f6dd8701fef4159d22fef3d9f3155106
Adam Lahbib <37028131+adamlahbib@users.noreply.github.com>
2022-12-13 13:18:40 +0100

    Update 2.c

--- ex/2.c
+++ ex/2.c
@@ -1,129 +1,175 @@
+
 #include <time.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#define TrucksNumber 4
-#define CarsNumber 8
+#define TrucksNumber	5
+#define CarsNumber	5
 #define TotalVehicles  (TrucksNumber+CarsNumber)
 
 
-#define waiting 1
-#define crossing 3
-#define other 2
+#define Waiting 1
+#define Other 2
+#define Crossing 3
 
 
-// semaphores
-pthread_mutex_t mutex;
+void wait(double max);
+int randomDraw(double max);
+
+
+void* car(void* arg);
+void* truck(void* arg);
+
+pthread_mutex_t sc;
+
+
 sem_t sempriv[TotalVehicles];
 
-int state[TotalVehicles]; // state of each vehicle
-int blockedTrucks=0; // number of blocked trucks
-int threshold=0; // sum of the tons of the vehicles crossing the bridge
 
-// random number generator
-int randomDraw(double m){
-	int r=(int) (m*rand()/(RAND_MAX+1.0));
-	if(r<1)
-		r=1;
-	return r;
-}
+int state[TotalVehicles];
 
-// wait for a random time
-void wait(double m){
-	struct timespec delay;
-	delay.tv_sec=randomDraw(m);
-	delay.tv_nsec=0;
-	nanosleep(&delay,NULL);
-}
+int blockedTrucks=0;
 
 
-// bridge
-void accessBridge(int tons, int id){
-	pthread_mutex_lock(&mutex);
-	// if the bridge supports the vehicle weight, vehicle can cross
-	if(threshold+tons<=15){
-		state[id]=crossing;
-		threshold+=tons;
-		sem_post(&sempriv[id]);
-	} else {
-		// if the vehicle is a truck, it can block the bridge
-		if(tons==15){
+int threshold=0;
+
+
+
+
+void accessBridge(int tons, int id) 
+{
+	pthread_mutex_lock(&sc);
+
+	if(threshold+tons <= 15)
+		{
+            threshold=threshold+tons;
+            state[id]=Crossing;
+            sem_post(&sempriv[id]);
+		}
+	else	{
+		state[id]=Waiting;
+		if(tons==15)
 			blockedTrucks++;
-		} 
-		// if the vehicle is a car, it can't block the bridge
-		state[id]=waiting;
-	}
-	pthread_mutex_unlock(&mutex);
+		}
+	
+	pthread_mutex_unlock(&sc);
 	sem_wait(&sempriv[id]);
 }
 
-void leaveBridge(int tons, int id){
+
+
+void leaveBridge(int tons, int id)
+{
 	int i;
-	pthread_mutex_lock(&mutex);
-	state[id]=other;
-	threshold-=tons;
-	// verify if there are blocked trucks
-	while((i<TotalVehicles)&&(threshold==0)){
-		if(state[i]==waiting){
-			threshold=15;
-			blockedTrucks--;
-			sem_post(&sempriv[i]);
-		}
-		i++;
-	}
 
-	for(i=TrucksNumber; i<TotalVehicles; i++){
-		if(state[i]==waiting && threshold<15 && blockedTrucks==0){
-			threshold+=5;
-			sem_post(&sempriv[i]);
-		}
-	}
-	pthread_mutex_unlock(&mutex);
+	pthread_mutex_lock(&sc);
+
+	state[id]=Other;
+	threshold=threshold-tons;
+
+	for(i=0; i<TrucksNumber;i++)
+		if ( (state[i]==Waiting) && (threshold==0) )
+			{
+                sem_post(&sempriv[i]);
+                threshold=15;
+                blockedTrucks--;
+			}
+
+	for(i=TrucksNumber; i<TotalVehicles;i++)
+		if ( (threshold<15) && (blockedTrucks==0)
+			&& (state[i]==Waiting) )
+			{
+                threshold=threshold+5;
+                sem_post(&sempriv[i]);
+			}
+
+	pthread_mutex_unlock(&sc);
 }
 
-// threads
-void* car(void* arg){
-	wait(5);
-	accessBridge(5, *((int*)arg));
-	printf("car %d is crossing the bridge\n",*((int*)arg));
-	wait(5);
-	printf("car %d has left the bridge\n",*((int*)arg));
-	leaveBridge(5, *((int*)arg));
-	pthread_exit(NULL);
-}
 
-void* truck(void* arg){
-	wait(5);
-	accessBridge(15, *((int*)arg));
-	printf("truck %d is crossing the bridge\n",*((int*)arg));
-	wait(5);
-	printf("truck %d has left the bridge\n",*((int*)arg));
-	leaveBridge(15, *((int*)arg));
 
-	pthread_exit(NULL);
-}
 
-// main
-void main(){
+
+int main()
+{
 	int i;
 	pthread_t id;
 
-	for (i=0; i<TotalVehicles; i++){
-		state[i]=other;
-		sem_init(&sempriv[i],0,0);
-	}
 
-	pthread_mutex_init(&mutex,NULL);
+	for(i=0; i<TotalVehicles;i++)
+			{
+                state[i]=Other;
+                sem_init(&sempriv[i],0,0);
+			}
 
-	for(i=0; i<TotalVehicles;i++){
-		int* r=(int*)malloc(sizeof(int));
-		*r=i;
-		if (i<TrucksNumber)
-			pthread_create(&id,NULL,truck,r);
-		else	pthread_create(&id,NULL,car,r);
-	}
+	pthread_mutex_init(&sc,0);
 	
+	for(i=0; i<TotalVehicles;i++)
+			{
+			int* j=(int*)malloc(sizeof(int));
+			*j=i;
+			if (i<TrucksNumber)
+				pthread_create(&id,NULL,truck,j);
+			else	
+                pthread_create(&id,NULL,car,j);
+			}
+
+
+	pthread_exit(NULL);
+};
+
+
+
+void* car(void* arg)
+{
+	int id=*((int*)arg);
+
+	wait(5.0);
+	accessBridge(5, id);
+	printf("car %d is crossing the bridge\n",id);
+	wait(5.0);
+	printf("car %d has left the bridge\n",id);
+	leaveBridge(5, id);
+
 	pthread_exit(NULL);
 }
+
+void* truck(void* arg)
+{
+	int id=*((int*)arg);
+
+	wait(5.0);
+	accessBridge(15, id);
+	printf("truck %d is crossing the bridge\n",id);
+	wait(5.0);
+	printf("truck %d has left the bridge\n",id);
+	leaveBridge(15, id);
+
+	pthread_exit(NULL);
+}
+
+
+
+
+
+int randomDraw(double m)
+{
+        int j=(int) (m*rand()/(RAND_MAX+1.0));
+        if(j<1)
+                j=1;
+        return j;
+}
+
+
+
+void wait(double m)
+{
+        struct timespec delay;
+
+        delay.tv_sec=randomDraw(m);
+        delay.tv_nsec=0;
+        nanosleep(&delay,NULL);
+}
+
